b7393c9f03f317ce02b44d6690b262693a2e3ca6
ASLAN <ammaraslan@gmail.com>
2022-02-09 15:32:01 +0300

    Gelişme kodlarına başlandı.

---
+++ Hafta_1/Gelisme_Operatorler.py
@@ -0,0 +1,59 @@
+# Aritmetik Operatörler
+# Toplama Operatörü (+)
+x = 5
+y = 10
+z = x + y
+print(z)
+
+# Çıkarma Operatörü (-)
+x = 9
+y = 10
+z = x - y
+print(z)
+
+# Çarpma Operatörü (*)
+carpma = (x + y) * (x - y)
+print(carpma)
+0
+# Bölme Operatörü (/)
+bolme = (x + y) / (x - y)
+print(bolme)
+
+# Mod Operatörü (%)
+Mod = 9 % 2
+print(Mod)
+
+# Tam Bölme Operatörü (//)
+TB = 7 // 3
+print(TB)
+
+# Üs Alma Operatörü (**)
+us = 7 ** 2
+print(us)
+
+# ust =
+ust = pow(7, 2)
+print(ust)
+
+# Yuvarlama Operatörü (round)
+deger = 7.55454
+print(round(deger))
+
+# Karşılaştırma Operatörler
+a = 5
+b = 5
+print(a == b)  # Eşittir
+print(a != b)  # Eşit değildir
+print(a <= b)  # Küçük Eşittir
+print(a < b)   # Küçük
+print(a >= b)  # Büyük Eşittir
+print(a > b)   # Büyük
+
+# Mantıksal Operatörler
+a = 5
+b = 5
+c = 6
+d = 7
+print(a == b or c == d)
+print(a == b and c == d)
+print(not(5 == 5))
---
+++ Hafta_1/_Gelisme_Uygulama_1.py
@@ -0,0 +1,7 @@
+""" Vize%40+Finalin%60 sonucu 60 durumuna göre geçti veya kaldı denilmesi """
+
+vize_notu = int(input("Vize Notunuzu Giriniz"))
+final_notu = int(input("Final Notunuzu Giriniz"))
+ort= vize_notu*0.4 + final_notu*0.6
+
+print(ort)
